4cd8f4ac1a6ca3ef08185b2fc16fd4593f1d3217
Samet ışık <sametreiz123asdf@gmail.com>
2018-01-24 18:16:18 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+var api = "http://smtl.me:8117";
+
+var coinUnits = 5;
+
+var poolHost = "smtl.me";
+
+var irc = "";
+
+var email = "";
+
+var cryptonatorWidget = ["BCN-BTC", "BCN-USD", "BCN-EUR""];
+
+var easyminerDownload = "https://github.com/zone117x/cryptonote-easy-miner/releases/";
+
+var blockchainExplorer = "https://chainradar.com/bcn/blocks";
+
+var transactionExplorer = "https://chainradar.com/bcn/transaction/";
